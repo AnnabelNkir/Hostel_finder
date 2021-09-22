355b9335f6c5c276955560bf434cc84b2a6c4b14
feddykip <ferdinand.kiplangat@student.moringaschool.com>
2021-09-22 16:34:45 +0300

    content upload on contacts

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function sendMessage() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+    
+    if(name.length === 0){
+        alert("Enter your name!");
+        throw new Error;
+    }
+    else if(email.length === 0){
+        alert("Enter your email!");
+        throw new Error;
+    }
+    else if(message.length === 0){
+        alert("Write your message!");
+        throw new Error;
+    }
+    else{
+        alert("Hey " + name + ", We have received your message. Thank you for reaching out to us.")
+    }
+  }
